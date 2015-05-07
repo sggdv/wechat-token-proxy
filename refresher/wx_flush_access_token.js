551644d4ed9700fbdcb4d77ad3be6d9435388b04
Kim <331599158@qq.com>
2015-05-07 17:50:28 +0800

    fixed

--- refresher/wx_flush_access_token.js
+++ refresher/wx_flush_access_token.js
@@ -1,5 +1,5 @@
 var request = require('request');
-var wx_config = require('../wx_config');
+var wx_config = require('../conf/wx_config');
 
 var url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential';
 url += '&appid=' + wx_config.app_id;
